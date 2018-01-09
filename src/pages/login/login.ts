1a2857f70922f09a7fe655f20e565598eb0735fd
Hallison Melo <hallss93@hotmail.com>
2018-01-09 21:16:08 -0200

    Criação do projeto. Início do template Login. Cores definidas. Logo feita com css (iniciado).

---
+++ src/pages/login/login.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+import { Storage } from '@ionic/storage';
+import { IonicPage, NavController, LoadingController, NavParams, ToastController } from 'ionic-angular';
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
+
+/**
+ * Generated class for the LoginPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-login',
+  templateUrl: 'login.html',
+})
+export class LoginPage {
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad LoginPage');
+  }
+
+}
